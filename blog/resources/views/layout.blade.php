164dd1bb6cbcbfac0bb96e8c8c58e87afad01b2b
Raryel <raryel.csouza@protonmail.com>
2022-07-28 14:26:38 -0300

    Css/JS imports no cabeçalho do template

---
+++ blog/resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!-- layout.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SignoWeb Teste</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <script src="{{ asset('js/jquery-3.6.0.min.js') }}" defer></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
+</head>
+<body>
+  <div class="container">
+    
+    @yield('content')
+  </div>
+  
+</body>
+</html>
