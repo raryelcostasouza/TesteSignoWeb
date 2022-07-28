468944c7a3ebc6a540ba3c403f7514f7dc08ef02
Raryel <raryel.csouza@protonmail.com>
2022-07-28 14:23:11 -0300

    Migration das tabelas no banco

---
+++ blog/database/migrations/2022_07_26_220253_create_enquetes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enquetes', function (Blueprint $table) {
+            $table->id();
+            $table->date('data_inicio');
+            $table->date('data_termino');
+            $table->string('titulo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enquetes');
+    }
+};
